605da278035c923aed65749401d9fc33e46d069e
Sarfraz Alam <alamsarfraz422@gmail.com>
2023-11-04 21:21:05 +0530

    Init

---
+++ classifier.py
@@ -0,0 +1,59 @@
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import math
+from mediapipe.framework.formats import landmark_pb2
+
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+mp_drawing_styles = mp.solutions.drawing_styles
+
+class GestureDetection:
+    
+    def __init__(self) -> None:
+        base_options = python.BaseOptions(model_asset_path='./gesture_recognizer_2.task')
+        options = vision.GestureRecognizerOptions(base_options=base_options)
+        self.recognizer = vision.GestureRecognizer.create_from_options(options)
+        print(self.recognizer)
+
+    
+    def createImage(self, image):
+        image = mp.Image(
+            image_format=mp.ImageFormat.SBGRA,
+            data= image
+        )
+        
+        return image
+    
+    def recognize_gesture(self, image):
+        return self.recognizer.recognize(image)
+    
+    def get_landmarks(self, recognition_result):
+        top_gesture = recognition_result.gestures[0][0]
+        hand_landmarks = recognition_result.hand_landmarks
+        
+        return (top_gesture, hand_landmarks)
+
+    def annotate(self, image, results):
+        mp_drawing = mp.solutions.drawing_utils
+        multi_hand_landmarks_list = results[1]
+        annotated_image = image.copy()
+        
+        for hand_landmarks in multi_hand_landmarks_list:
+            hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            hand_landmarks_proto.landmark.extend([
+                landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+            ])
+
+            mp_drawing.draw_landmarks(
+                annotated_image,
+                hand_landmarks_proto,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+            pass        
+        
+        
+        return annotated_image
---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import cv2
+import keras
+import numpy as np
+import classifier
+
+st.title('Sign Recognition')
+
+# picture = st.camera_input("Take a picture")
+
+model = classifier.GestureDetection()
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+
+# def createModal():
+#     model = keras.models.load_model('./model.h5')
+#     return model
+
+# model = createModal()
+camera = cv2.VideoCapture(0)
+
+while run:
+    _, opencv_image = camera.read()
+    
+    opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+    
+    
+    arr = np.asarray(opencv_image)
+    
+    img = model.createImage(arr)
+    
+    try:
+    
+        results = model.recognize_gesture(img)
+        landmarks = model.get_landmarks(results)        
+        annotated_image = model.annotate(opencv_image, landmarks)    
+        cv2.putText(annotated_image, landmarks[0].category_name, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        FRAME_WINDOW.image(annotated_image, channels="RGB")
+    except:
+        FRAME_WINDOW.image(opencv_image, channels="RGB")
+
+    
+    
